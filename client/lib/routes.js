f6f3dd7895178bb943fc9f15f80fb17b7b9b839f
Lukas Jakob Hafner <luki@booncon.com>
2014-12-02 10:57:08 +0200

    fixed search route

--- client/lib/routes.js
+++ client/lib/routes.js
@@ -2,6 +2,9 @@
 Router.route('/', function () {
   this.render('tracklist');
 });
+Router.route('/search/', function () {
+  this.render('search', {data: {query: this.params._query}});
+});  
 Router.route('/search/:_query', function () {
   // this.layout('application');
   // this.render('search', {to: 'overlay', data: {query: this.params._query}});
